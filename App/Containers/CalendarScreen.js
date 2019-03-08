6f4721e9ba53710e1251fc4e94a4ba5dea21b347
prabhaav <prabhaav.bhardwaj@gmail.com>
2019-03-07 22:14:49 -0800

    adding calendar screen

--- App/Containers/CalendarScreen.js
+++ App/Containers/CalendarScreen.js
@@ -1,127 +1,124 @@
 import React, { Component } from 'react'
-import { View } from 'react-native';
-import {
-  RkButton,
-  RkCalendar,
-  RkStyleSheet,
-} from 'react-native-ui-kitten';
 import { connect } from 'react-redux'
-// Add Actions - replace 'Your' with whatever your reducer is called :)
-// import YourActions from '../Redux/YourRedux'
-
-// Styles
+import {
+  Text,
+  View,
+  StyleSheet,
+  TouchableOpacity
+} from 'react-native';
+import {Agenda} from 'react-native-calendars';
+import NavigationType from '../Navigation/propTypes';
 
 class CalendarScreen extends Component {
-  static navigationOptions = {
-    title: 'Horizontal Calendar',
+  static propTypes = {
+    navigation: NavigationType.isRequired,
   };
 
-  state = {
-    min: new Date(2018, 0, 1),
-    max: new Date(2019, 0, 1),
-    visibleMonth: undefined,
+  static navigationOptions = ({ navigation }) => {
+    const params = navigation.state.params || {}
+    return {
+      headerTitle: 'Events'.toUpperCase(),
+      headerBackTitle: 'Back',
+      headerTintColor: 'black',
+    }
   };
 
-  calendarRef = undefined;
-
-  onPrevButtonPress = () => {
-    this.calendarRef.scrollToPreviousMonth();
-  };
-
-  onNextButtonPress = () => {
-    this.calendarRef.scrollToNextMonth();
-  };
-
-  onTodayButtonPress = () => {
-    this.calendarRef.scrollToToday();
-  };
-
-  onVisibleMonthChanged = (date) => {
-    this.setState({
-      visibleMonth: date,
-    });
-  };
-
-  setCalendarRef = (ref) => {
-    this.calendarRef = ref;
-  };
-
-  isSameMonth = (date1, date2) => {
-    const isNotNull = date1 && date2;
-    return isNotNull
-      && date1.getFullYear() === date2.getFullYear()
-      && date1.getMonth() === date2.getMonth();
-  };
-
-  isPrevButtonEnabled = () => !this.isSameMonth(this.state.visibleMonth, this.state.min);
-
-  isNextButtonEnabled = () => !this.isSameMonth(this.state.visibleMonth, this.state.max);
-
-  renderControlButtons = () => {
-    const prevButtonStyle = this.isPrevButtonEnabled() ? {} : { opacity: 0.5 };
-    const nextButtonStyle = this.isNextButtonEnabled() ? {} : { opacity: 0.5 };
-    return (
-      <View style={styles.controlContainer}>
-        <RkButton
-          disabled={this.isSameMonth(this.state.visibleMonth, this.state.min)}
-          style={[styles.controlButton, prevButtonStyle]}
-          rkType='stretch success'
-          onPress={this.onPrevButtonPress}>
-          {'<'}
-        </RkButton>
-        <RkButton
-          style={styles.controlButton}
-          rkType='success'
-          onPress={this.onTodayButtonPress}>
-          TODAY
-        </RkButton>
-        <RkButton
-          disabled={this.isSameMonth(this.state.visibleMonth, this.state.max)}
-          style={[styles.controlButton, nextButtonStyle]}
-          rkType='stretch success'
-          onPress={this.onNextButtonPress}>
-          {'>'}
-        </RkButton>
-      </View>
-    );
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: {}
+    };
+  }
 
   render() {
     return (
-      <View style={styles.container}>
-        {this.renderControlButtons()}
-        <RkCalendar
-          ref={this.setCalendarRef}
-          layout='horizontal'
-          min={this.state.min}
-          max={this.state.max}
-          onVisibleMonthChanged={this.onVisibleMonthChanged}
-        />
-      </View>
+      <Agenda
+        items={this.state.items}
+        loadItemsForMonth={this.loadItems.bind(this)}
+        selected={'2019-03-08'}
+        renderItem={this.renderItem.bind(this)}
+        renderEmptyDate={this.renderEmptyDate.bind(this)}
+        rowHasChanged={this.rowHasChanged.bind(this)}
+        // markingType={'period'}
+        // markedDates={{
+        //    '2019-03-08': {textColor: '#666'},
+        //    '2019-03-09': {textColor: '#666'},
+        //    '2019-03-14': {startingDay: true, endingDay: true, color: '#90ee90'},
+        //    '2019-03-21': {startingDay: true, color: '#90ee90'},
+        //    '2019-03-22': {endingDay: true, color: '#ffcba4'},
+        //    '2019-03-24': {startingDay: true, color: '#ffcba4'},
+        //    '2019-03-25': {color: '#ffcba4'},
+        //    '2019-03-26': {endingDay: true, color: '#ffcba4'}}}
+         // monthFormat={'yyyy'}
+         // theme={{calendarBackground: 'red', agendaKnobColor: 'green'}}
+        //renderDay={(day, item) => (<Text>{day ? day.day: 'item'}</Text>)}
+      />
     );
+  }
+
+  loadItems(day) {
+    setTimeout(() => {
+      for (let i = -15; i < 85; i++) {
+        const time = day.timestamp + i * 24 * 60 * 60 * 1000;
+        const strTime = this.timeToString(time);
+        if (!this.state.items[strTime]) {
+          this.state.items[strTime] = [];
+          const numItems = Math.floor(Math.random() * 5);
+          for (let j = 0; j < numItems; j++) {
+            this.state.items[strTime].push({
+              name: 'Agenda for ' + strTime,
+              height: Math.max(50, Math.floor(Math.random() * 150))
+            });
+          }
+        }
+      }
+      //console.log(this.state.items);
+      const newItems = {};
+      Object.keys(this.state.items).forEach(key => {newItems[key] = this.state.items[key];});
+      this.setState({
+        items: newItems
+      });
+    }, 1000);
+    // console.log(`Load Items for ${day.year}-${day.month}`);
+  }
+
+  renderItem(item) {
+    return (
+      <View style={[styles.item, {height: item.height}]}><Text>{item.name}</Text></View>
+    );
+  }
+
+  renderEmptyDate() {
+    return (
+      <View style={styles.emptyDate}><Text>This is empty date!</Text></View>
+    );
+  }
+
+  rowHasChanged(r1, r2) {
+    return r1.name !== r2.name;
+  }
+
+  timeToString(time) {
+    const date = new Date(time);
+    return date.toISOString().split('T')[0];
   }
 }
 
-const styles = RkStyleSheet.create(theme => ({
-  container: {
+const styles = StyleSheet.create({
+  item: {
+    backgroundColor: 'white',
     flex: 1,
-    backgroundColor: theme.colors.background,
-    paddingHorizontal: 8,
+    borderRadius: 5,
+    padding: 10,
+    marginRight: 10,
+    marginTop: 17
   },
-  controlContainer: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-    marginVertical: 2,
-    backgroundColor: theme.colors.highlight,
-  },
-  controlButton: {
-    marginHorizontal: 8,
-    paddingHorizontal: 16,
-  },
-  controlButtonText: {
-
-  },
-}));
+  emptyDate: {
+    height: 15,
+    flex:1,
+    paddingTop: 30
+  }
+});
 
 const mapStateToProps = (state) => {
   return {
