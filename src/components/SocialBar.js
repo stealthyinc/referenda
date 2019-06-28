eb1af8cfea8403690e9639e3e7cabec19bd7b2b2
prabhaav <prabhaav.bhardwaj@gmail.com>
2019-06-27 20:24:24 -0700

    fix wrong linking lib

--- src/components/SocialBar.js
+++ src/components/SocialBar.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import {
   View,
+  Linking
 } from 'react-native';
 import {
   CardItem,
   Button,
   Icon,
-  Text,
-  Linking
+  Text
 } from 'native-base'
 import {
   Amplitude,
